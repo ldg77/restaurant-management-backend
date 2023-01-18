fbf9261c06ad2a5ea53d2eaa5e62af2f3decd9e8
Dumitru Gurduza <dimulya@gmail.com>
2023-01-18 14:08:59 +0100

    add validation

--- middleware/hashPWD.js
+++ middleware/hashPWD.js
@@ -1,10 +1,16 @@
 import bcrypt from "bcrypt";
 export default async (req, res, next) => {
   try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    req.body.password = hash;
-
-    next();
+    console.log(req.body.password === req.body.repeatPassword);
+    if (req.body.password === req.body.repeatPassword) {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hash;
+      next();
+    } else {
+      res
+        .status(404)
+        .send({ message: false, message: "password werification fail" });
+    }
   } catch (error) {
     res.status(404).send({ message: false, message: "something wrong" });
   }
